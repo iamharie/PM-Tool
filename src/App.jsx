d72eec70e437b16618bda77f1c55316b0377a185
iamharie <haranhari350@gmail.com>
2024-08-26 13:33:35 -0500

    added comp:3 and 4 - Button and NoProject

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,48 @@
 import NewProject from "./components/NewProject.jsx";
 import ProjectSidebar from "./components/ProjectsSidebar.jsx";
+import NoProjectSelected from "./components/NoProjectSelected.jsx";
+import { useState } from "react";
 
 function App() {
+  const [projectsState, setprojectsState] = useState({
+    selectedProjectId: undefined,
+    projects: [],
+  });
+
+  function handleStartAddProject() {
+    setprojectsState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: null,
+      };
+    });
+    console.log("clicked");
+  }
+
+  function handleAddProject(projectDate) {
+    setprojectsState((prevState) => {
+      const newProject = {
+        ...projectDate,
+        id: Math.random(),
+      };
+      return {
+        ...prevState,
+        projects: [...prevState.projects, newProject],
+      };
+    });
+  }
+  console.log(projectsState);
+  let content;
+  if (projectsState.selectedProjectId === null) {
+    content = <NewProject onAdd={handleAddProject} />;
+  } else if (projectsState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
+  }
   return (
     <main className="h-screen my-8 flex gap-8">
-      {/* <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1> */}
-      <ProjectSidebar />
-      <NewProject />
+      <ProjectSidebar onStartAddProject={handleStartAddProject} />
+      {/* <NoProjectSelected onStartAddProject={handleStartAddProject} /> */}
+      {content}
     </main>
   );
 }
