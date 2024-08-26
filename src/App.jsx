78d81a8d80206f0964a8a06614c267f654b3247e
iamharie <haranhari350@gmail.com>
2024-08-26 12:41:43 -0500

    added comp:2 - NewProject --> Input | Styled

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import ProjectSidebar from "./component/ProjectsSidebar";
+import NewProject from "./components/NewProject.jsx";
+import ProjectSidebar from "./components/ProjectsSidebar.jsx";
 
 function App() {
   return (
-    <main className="h-screen my-8">
-      <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
+    <main className="h-screen my-8 flex gap-8">
+      {/* <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1> */}
       <ProjectSidebar />
+      <NewProject />
     </main>
   );
 }
